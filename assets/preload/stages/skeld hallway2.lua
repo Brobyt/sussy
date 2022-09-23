4537b494fe5631eda16f71d66f820d8f4c590277
Brobyt <112826210+Brobyt@users.noreply.github.com>
2022-09-22 21:13:49 -0300

    Add files via upload

---
+++ assets/preload/stages/skeld hallway2.lua
@@ -0,0 +1,15 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('skeld hallway2', 'skeld hallway', -500, -400);
+	setScrollFactor('skeld hallway2', 0.9, 0.9);
+	scaleObject('skeld hallway2', 1.1, 1.1);
+	addLuaSprite('skeld hallway2', false);
+	makeAnimatedLuaSprite('crewmate', 'crewmate', -300, -350)
+	setScrollFactor('crewmate', 0.9, 0.9);
+	scaleObject('crewmate', 1.1, 1.1);
+	 addAnimationByPrefix('crewmate', 'idle', 'crewmate idle', 24, true)
+	 addLuaSprite('crewmate', true)
+end
+function onBeatHit()
+objectPlayAnimation('crewmate', 'idle', true);
+end
