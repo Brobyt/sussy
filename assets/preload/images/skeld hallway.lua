32ab25a672e909a24bb74b322244d72f75b9b120
Brobyt <112826210+Brobyt@users.noreply.github.com>
2022-09-22 21:10:37 -0300

    Add files via upload

---
+++ assets/preload/images/skeld hallway.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('skeld hallway', 'skeld hallway', -500, -400);
+	setScrollFactor('skeld hallway', 0.9, 0.9);
+	scaleObject('skeld hallway', 1.1, 1.1);
+	addLuaSprite('skeld hallway', false);
+end
